ef89bb0931f684359cd754df090da4bea919ef08
NISHANT <thakurnishant0420@gmail.com>
2024-02-17 18:44:58 +0530

    adding function

---
+++ function.js
@@ -0,0 +1,9 @@
+// type 1
+function add(a,b){
+    s=a+b;
+    console.log(s)
+}
+add(69,69)
+console.log(s)
+// type 2
+
--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ console.log(typeof myname);
 console.log(typeof(myname));
 // == is only value === for value and type both
 // node --watch jsfilename for auto run
-// let makes scope define
+// let makes scope define let and var both work as same but var purana hai.
 
 
 console.log(1 == "1")
