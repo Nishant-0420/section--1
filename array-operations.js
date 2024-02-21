f64a676d0f6efc665d873015899f25ff88303e9e
NISHANT <thakurnishant0420@gmail.com>
2024-02-21 18:32:32 +0530

    js array questions

--- array-operations.js
+++ array-operations.js
@@ -13,7 +13,10 @@ for(let j of num1){
 }
 console.log(c)
 console.log("------------------");
-// forEach loop
+// forEach loop we have arrow function over here and can pass 2 parameters 1st for array element 
+// 2nd element for index and we can use them . only want to print arr element just add console.log() under fun
+// but want to apply other functions like adding or etc just print them outside the fun. and generate variable outside the function and use it under fun
+
 let q=0
 num1.forEach((a,indexx)=>{ q+=a;console.log(indexx)})
 console.log(q)
@@ -29,3 +32,21 @@ num2.forEach((n,e)=>{
     }
 })
 console.log(u);
+console.log("cube")
+// printing cude if element
+num2.forEach((a)=>{
+    console.log(a**3)
+})
+
+// wap to add all even element
+console.log("sum of even no.")
+num2=[1,2,3,4,5,6,7,8]
+let x=0
+
+num2.forEach((s)=>{
+    if(s%2===0){
+        x+=s
+    }
+})
+console.log(x)
+
